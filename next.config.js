9253776ac570a9f6cac3c6866f59ac7d3974e211
gkdk4195 <gkdk4195@gmail.com>
2025-03-25 10:52:03 +0900

    chore: Deprecated 설정을 최신api로 마이그레이션

--- next.config.js
+++ next.config.js
@@ -1,5 +1,28 @@
 module.exports = {
-  images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
-  },
-}
+	images: {
+	  remotePatterns: [
+		{
+		  protocol: 'https',
+		  hostname: 'www.notion.so',
+		  pathname: '/image/**',
+		},
+		{
+		  protocol: 'https',
+		  hostname: 'lh3.googleusercontent.com',
+		},
+		{
+		  protocol: 'https',
+		  hostname: 'lh5.googleusercontent.com',
+		},
+		{
+		  protocol: 'https',
+		  hostname: 's3-us-west-2.amazonaws.com',
+		},
+		{
+		  protocol: 'https',
+		  hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+		},
+	  ],
+	},
+  }
+  
